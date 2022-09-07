9f46738d3fa871afc91e679aa8911be8053a51fb
Yekeen Toyyib <yekeentoyyib@gmail.com>
2022-09-07 10:23:28 +0100

    i made a change on the dashboard wallet

--- frontend/src/components/dashboard/Main.js
+++ frontend/src/components/dashboard/Main.js
@@ -140,11 +140,8 @@ const Main = () => {
                 wallet? wallet.map((item) => (
                   <li className="d-flex align-items-center col-6 pe-3 pe-sm-0">
                     <div className="bg-dark col-11 rounded-4">
-                      <div className="d-flex justify-content-between mx-2">
+                      <div className="d-flex justify-content-start mx-2">
                         <h3 className="text-white">{item.name}</h3>
-                        <button className="btn  text-white">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024"><path fill="white" d="M864 256H736v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zm-200 0H360v-72h304v72z"/></svg>
-                        </button>
                       </div>
                       <div className="d-flex justify-content-between mx-2">
                         <h3 className="text-white m-0">
